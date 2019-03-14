cfe6f5ce02c8c59e354430cbb7fa006e632e70aa
annapancioli <annapancioli@gmail.com>
2019-03-14 16:47:34 -0400

    user account and home components styled

---
+++ js/components/UserAccountComponent.js
@@ -0,0 +1,28 @@
+export default {
+	template: `
+	<div class="userAccountContainer row">
+			<div class="small-10 small-offset-1 medium-8 medium-offset-2 large-8 large-offset-2 columns text-center">
+				<h2 class="userATitle">Manage your account</h2>
+				<div id="userOptions" class="row">
+                    <nav class="small-10 small-offset-1 medium-8 medium-offset-2 large-8 large-offset-2 columns text-center">
+                        <ul id="editUser">
+                            <a href="#"><li>
+                                <i class="fas fa-user-plus fa-3x"></i>
+                                <h4 id="newUser">Create New User</h4>
+                            </li></a>
+                            <a href="#"><li>
+                                <i class="fas fa-edit fa-3x"></i>
+                                <h4>Edit Users</h4>
+                            </li></a>
+                            <a href="#"><li>
+                                <i class="fas fa-user-times fa-3x"></i>
+                                <h4>Delete User</h4>
+                            </li></a>
+                        </ul>
+                    </nav>  
+				</div> 
+			</div>  
+	</div>
+    `,
+
+}
