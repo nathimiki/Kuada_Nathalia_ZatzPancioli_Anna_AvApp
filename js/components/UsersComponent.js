9b62f58ce0b840f7386e50475d91665da3c49fd7
annapancioli <annapancioli@gmail.com>
2019-03-14 14:28:30 -0400

    login and user components styled

---
+++ js/components/UsersComponent.js
@@ -0,0 +1,14 @@
+export default {
+
+	template: `
+	<div class="usersContainer row">
+			<div class="small-10 small-offset-1 medium-8 medium-offset-2 large-8 large-offset-2">
+				<h2 class="usersTitle">Select your account</h2>
+				<div id="userProfiles">
+					<user></user>  
+				</div> 
+			</div>  
+	</div>
+    `,
+
+}
