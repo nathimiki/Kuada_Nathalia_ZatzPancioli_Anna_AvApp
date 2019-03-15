b8737f4407e0434a22dd3ddff65253667c749b3a
nathimiki <nathimiki@gmail.com>
2019-03-15 04:56:48 -0400

    edited user account and create components

--- js/components/UserAccountComponent.js
+++ js/components/UserAccountComponent.js
@@ -8,18 +8,18 @@ export default {
 				<div id="userOptions" class="row">
                     <nav class="small-10 small-offset-1 medium-8 medium-offset-2 large-8 large-offset-2 columns text-center">
                         <ul id="editUser">
-                            <a href="#"><li>
+                            <li v-on:click="createUser()">
                                 <i class="fas fa-user-plus fa-3x"></i>
                                 <h4 id="newUser">Create New User</h4>
-                            </li></a>
+                            </li>
                             <a href="#"><li>
                                 <i class="fas fa-edit fa-3x"></i>
                                 <h4>Edit Users</h4>
                             </li></a>
-                            <a href="#"><li>
+                            <li v-on:click="deleteUser('liveuser.id')">
                                 <i class="fas fa-user-times fa-3x"></i>
                                 <h4>Delete User</h4>
-                            </li></a>
+                            </li>
                         </ul>
                     </nav>  
 				</div> 
@@ -29,8 +29,58 @@ export default {
     
     data() {
         return {
-          message: `Manage your account`,
+          message: `Choose which user you want to edit:`,
+
+          userList: []
         }
       },
+
+    methods: {
+    fetchUsers() {
+        let url = `./admin/scripts/users.php?allusers=true`;
+        console.log('{this.userList = data}');
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {this.userList = data})
+        .catch(function(error) {
+        console.error(error);
+        });
+    },
+    
+    editUser(id){
+        this.$router.push({ name:"edituser", params: { currentuser: this.liveuser } });
+    },
+
+    createUser(){
+        this.$router.push({ name: "createuser" });
+    },
+
+    deleteUser(id){
+        let url =  `./admin/scripts/admin_deleteuser.php`;
+            const formData = new FormData();
+
+            formData.append("id", id);
+
+            fetch(url, {
+                    method: 'POST',
+                    body: formData
+            })
+            .then(res=>res.json())
+            .then(data =>{
+                if ( data != "Success!!"){
+                    console.warn(data);
+                    console.log('Something went wrong!');
+                    return;
+                }else{
+                    console.log('User deleted successfully');
+                    this.fetchUsers();
+                }
+            })
+            .catch(function(error){
+                    console.log(error);
+            });
+        }
+
+    }
     
 }
--- js/components/UserCreateComponent.js
+++ js/components/UserCreateComponent.js
@@ -6,21 +6,98 @@ export default {
         <div class="newUser small-10 medium-8 large-8 columns text-center">
         <h2>Create New User</h2>
             <form>
-                <input type="text" id="fname" placeholder="fname" required>
-                <input type="text" id="name" placeholder="username" required>
-                <input type="email" id="email" placeholder="email" required>
-                <input type="password" id="password" placeholder="password" required>
-                <select>
+                <input v-model="input.fname" type="text" id="fname" placeholder="fname" required>
+                <input v-model="input.username" type="text" id="name" placeholder="username" required>
+                <input v-model="input.email" type="email" id="email" placeholder="email" required>
+                <input v-model="input.password" type="password" id="password" placeholder="password" required>
+                <select v-model="input.rating">
                     <option disable selected>rating</option>
-                    <option>Rating Type</option>
+                    <option v-for="rate in rating" v-bind:value="rate.rating_id">{{rate.rating_type}}</option>
                 </select>
-                <input type="checkbox" id="adminBox">
+                <input type="checkbox" id="adminBox" v-model="input.admin">
                 <label for="admin">
                     <p>Is this user an admin?</p>
                 </label><br>
-                <button type="submit">Create User</button>
+                <button v-on:click.prevent="submit()" type="submit">Create User</button>
             </form>  
         </div>          
     </div>`,
 
+    data() {
+        return {
+            input: {
+                fname: '',
+                username: '',
+                email: '',
+                password: '',
+                rating: '',
+                admin: false,
+            },
+
+            adminNumber: 0,
+            rating: []
+        }
+    },
+
+    created: function() {
+        this.getRatings();
+    },
+
+    methods: {
+        getRatings(){
+
+            let url = `./admin/scripts/rating.php?allRatings=true`;
+
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {this.rating = data})
+            .catch(function(error) {
+                console.error(error);
+            });
+
+        },
+
+        submit(){
+            if(this.input.fname != "" && this.input.username != "" && this.input.email != "" && this.input.password != "" && this.input.rating !=""){
+
+                let url =  `./admin/scripts/admin_createuser.php`;
+
+                if (this.input.admin == true){
+                    this.input.admin = 1;
+                }else{
+                    this.input.admin = 0;
+                }
+
+                const formData = new FormData();
+
+                formData.append("fname", this.input.fname);
+                formData.append("name", this.input.username);
+                formData.append("email", this.input.email);
+                formData.append("password", this.input.password);
+                formData.append("rating", this.input.rating);
+                formData.append("admin", this.input.admin);
+
+
+                fetch(url, {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(res=>res.json())
+                .then(data =>{
+                    if (data != "User created successfully!"){
+                        console.warn(data);
+                        console.log('Something went wrong!');
+                        return;
+                    }else{
+                        this.$router.replace({name:'account'});
+                    }
+                })
+                .catch(function(error){
+                    console.log(error);
+                });
+            }else{
+                console.log('Fill in required fields!');
+            }
+        }
+    }
 }
